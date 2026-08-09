14aa3fea7795e6c378a77e47a1410a16c498637d
Dionysus <158821418+Cybeles-Eos@users.noreply.github.com>
2026-08-09 21:12:39 +0800

    first commit

---
+++ assets/dialog.js
@@ -0,0 +1,56 @@
+window.__DIALOG__ = {
+    fromMe: [{
+        id: '0000',
+        details: [
+            [
+                "💐💐💐Happy 4th Monthsary💐💐💐",
+                'My Ella Cute 😘',
+                '🌹🌹🌹Flowers for you🌹🌹🌹',
+                '<pre>🍀🍀🍀🌹🍀🍀🍀🌹🍀🍀🍀 \n🍀🍀🌹🌹🌹🍀🌹🌹🌹🍀🍀 \n🍀🌹🌹🌹🌹🌹🌹🌹🌹🌹🍀 \n🍀🌹🌹🌹🌹🌹🌹🌹🌹🌹🍀 \n🌹🌹🌹🌹🌹🌹🌹🌹🌹🌹🌹 \n🍀🌹🌹🌹🌹🌹🌹🌹🌹🌹🍀 \n🍀🍀🌹🌹🌹🌹🌹🌹🌹🍀🍀 \n🍀🍀🍀🌹🌹🌹🌹🌹🍀🍀🍀 \n🍀🍀🍀🍀🍀🌹🍀🍀🍀🍀🍀</pre>',
+                "Haven't seen you in days 📅",
+                'I miss you so much 😘',
+                "<img src='img/me-andyou.png'>",
+                'You said you hate long-distance relationships 🤔',
+                'Then a nationwide outbreak of a new pneumonia 😷',
+                "<img src='img/separate.jpeg'>",
+                'Forced us into a long-distance relationship 😂',
+                "<img src='img/me-andyou.png'>",
+                'Wait a moment 🙂',
+                'Let me look at your photo one more time 😘',
+                "<img src='img/me-andyou.png'>",
+                'Every day we spent together 📅',
+                'Makes me miss you even more 📬',
+                "<img src='img/me-andyou.png'>",
+                'I miss the breakfasts you made 🍲',
+                "<img src='img/me-andyou.png'>",
+                'I miss working out together 🛹',
+                "<img src='img/me-andyou.png'>",
+                'I miss traveling with you 🚂',
+                "<img src='img/me-andyou.png'>",
+                'Our footprints side by side 🎠',
+                "<img src='img/me-andyou.png'>",
+                'The vow to be together forever 💑',
+                'I want to say it again 😍',
+                "<img src='img/marry-me.png'>",
+                'Will you marry me? 💍'
+            ]
+        ],
+        responses: [{
+            content: 'Yes, I will',
+            nextAuthor: ['0001']
+        }]
+    }, {
+        id: '0001',
+        details: [
+            [
+                "<img src='img/kiss-my-princess.png'>",
+                "I'm so happy, darling 😁",
+                'Every day we see each other from now on 😍',
+                "You'll always be my first love 😘",
+                'Every promise I made to you 😙',
+                "I'll keep them all 😉"
+            ]
+        ]
+    }],
+    fromUser: []
+};
